2e5648a19724f120fea5c70f3bf2c593bbbb84f3
Vlad-Dima21 <vladima2001@gmail.com>
2023-12-29 12:18:18 +0200

    Initial commit

---
+++ src/main/java/com/vladima/gamingrental/GamingRentalApplication.java
@@ -0,0 +1,13 @@
+package com.vladima.gamingrental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamingRentalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GamingRentalApplication.class, args);
+    }
+
+}
