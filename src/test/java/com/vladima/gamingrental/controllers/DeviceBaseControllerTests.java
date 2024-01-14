626e34b45c98bcc9e99b846741d0d5fb02938f90
Vlad-Dima21 <vladima2001@gmail.com>
2024-01-14 11:08:00 +0200

    deviceBase, device controllers unit test

---
+++ src/test/java/com/vladima/gamingrental/controllers/DeviceBaseControllerTests.java
@@ -0,0 +1,87 @@
+package com.vladima.gamingrental.controllers;
+
+import com.vladima.gamingrental.device.controllers.DeviceBaseController;
+import com.vladima.gamingrental.device.models.Device;
+import com.vladima.gamingrental.device.models.DeviceBase;
+import com.vladima.gamingrental.device.services.DeviceBaseServiceImpl;
+import com.vladima.gamingrental.helpers.EntityOperationException;
+import com.vladima.gamingrental.helpers.StringifyJSON;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.mockito.BDDMockito.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+
+@ExtendWith(MockitoExtension.class)
+@WebMvcTest(DeviceBaseController.class)
+public class DeviceBaseControllerTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private DeviceBaseServiceImpl service;
+
+    private DeviceBase ps5;
+
+    @BeforeEach
+    public void init() {
+        ps5 = new DeviceBase(
+                1L, "PS5", "Sony",
+                2018, List.of(new Device(2, true)),
+                List.of()
+        );
+    }
+
+    @Test
+    @DisplayName("Unit test for creating a new device with an already used name that returns an error json")
+    public void  whenExistingDeviceName_createDevice_returnErrorJSON() throws Exception {
+        var exception = new EntityOperationException(
+                "Device not added",
+                "A device with the same name already exists",
+                HttpStatus.CONFLICT
+        );
+        given(service.create(ps5.toDTO()))
+                .willThrow(exception);
+        mockMvc.perform(post("/api/devices/create")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(StringifyJSON.toJSON(ps5)))
+                .andDo(print())
+                .andExpect(status().isConflict())
+                .andExpect(jsonPath("$.message").value(exception.getMessage()));
+    }
+
+    @Test
+    @DisplayName("Unit test for fetching devices given a filter")
+    public void whenFilteredByName_getFilteredDevices_returnDevicesJSON() throws Exception {
+        var searchBy = ps5.getDeviceBaseName().substring(0,2);
+        given(service.getFiltered(searchBy, null, null, false))
+                .willReturn(List.of(ps5.toDTO()));
+        mockMvc.perform(get("/api/devices")
+                .param("name", searchBy))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].deviceBaseName").value(ps5.getDeviceBaseName()));
+    }
+
+    @Test
+    @DisplayName("Unit test for deleting a device")
+    public void whenValidDevice_deleteDevice_noContent() throws Exception {
+        mockMvc.perform(delete("/api/devices/remove/{id}", 1))
+                .andDo(print())
+                .andExpect(status().isNoContent());
+    }
+}
---
+++ src/test/java/com/vladima/gamingrental/controllers/DeviceControllerTests.java
@@ -0,0 +1,82 @@
+package com.vladima.gamingrental.controllers;
+
+import com.vladima.gamingrental.device.controllers.DeviceController;
+import com.vladima.gamingrental.device.models.Device;
+import com.vladima.gamingrental.device.models.DeviceBase;
+import com.vladima.gamingrental.device.services.DeviceServiceImpl;
+import com.vladima.gamingrental.helpers.EntityOperationException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.mockito.BDDMockito.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+
+@ExtendWith(MockitoExtension.class)
+@WebMvcTest(DeviceController.class)
+public class DeviceControllerTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private DeviceServiceImpl service;
+
+    private DeviceBase ps5;
+
+    private Device ps5Unit1;
+
+    @BeforeEach
+    public void init() {
+        ps5 = new DeviceBase(
+                1L, "PS5", "Sony",
+                2018, List.of(),
+                List.of()
+        );
+        ps5Unit1 = new Device(1L, 1, true, ps5, null);
+        Device ps5Unit2 = new Device(2L, 3, false, ps5, null);
+        ps5.setDevices(List.of(ps5Unit1, ps5Unit2));
+    }
+
+    @Test
+    @DisplayName("Unit test for fetching units based on the device id")
+    public void whenInvalidId_getByDeviceBaseId_returnErrorJSON() throws Exception {
+        var exception = new EntityOperationException(
+                "Device not found",
+                "Error fetching device with id 1",
+                HttpStatus.NOT_FOUND
+        );
+        given(service.getByDeviceBaseId(ps5.getDeviceBaseId()))
+                .willThrow(exception);
+
+        mockMvc.perform(get("/api/units/of-id/{id}", ps5.getDeviceBaseId()))
+                .andDo(print())
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.message").value(exception.getMessage()));
+    }
+
+    @Test
+    @DisplayName("Unit test for fetching units that are available")
+    public void whenFilteredByAvailability_getByDeviceBaseName_returnUnitsJSON() throws Exception {
+        given(service.getByDeviceBaseName(ps5.getDeviceBaseName(), true))
+                .willReturn(List.of(ps5Unit1.toDTO()));
+
+        mockMvc.perform(get("/api/units/of")
+                        .param("name", ps5.getDeviceBaseName())
+                        .param("availableOnly", String.valueOf(true)))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].deviceName").value(ps5.getDeviceBaseName()));
+    }
+}
